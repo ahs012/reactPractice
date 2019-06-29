046ccf3edbf3e9716e257ab4a8e0d0f5c87afcc3
Smoss <ahs012@gmail.com>
2019-06-29 13:56:02 -0400

    NavBar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-info">
+        <a className="navbar-brand" href="/">
+          React Practice   /
+        </a>
+        <a className = "navbar-brand" id = "" href = "/">
+          Button 2   /
+        </a>
+        <a className = "navbar-brand" id = "" href = "/">
+          Button 3
+        </a>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
+  
